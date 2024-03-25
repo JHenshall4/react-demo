91ad853a6d45a0745dd41844c4de0bc17489df2f
jhenshall4 <jimmyh543@hotmail.com>
2024-03-25 11:28:43 +0000

    Completed Section 3 - React Essentials: Components, JSX, Props, State & More.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton/TabButton";
 
 function App() {
+  const [tabContent, setTabContent] = useState("");
+
+  function handleClick(clickedButton) {
+    setTabContent(clickedButton);
+  }
   return (
     <div>
       <Header />
@@ -10,12 +17,47 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {/* Neat, I didn't realise we can use the spread operator for sending data to compoentns */}
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={tabContent === "components"}
+              onClick={() => handleClick("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "jsx"}
+              onClick={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "props"}
+              onClick={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "state"}
+              onClick={() => handleClick("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          {tabContent === "" && <p>Please select a topic.</p>}
+          {tabContent && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[tabContent].title}</h3>
+              <p>{EXAMPLES[tabContent].description}</p>
+              <code>{EXAMPLES[tabContent].code}</code>
+            </div>
+          )}
         </section>
       </main>
     </div>
